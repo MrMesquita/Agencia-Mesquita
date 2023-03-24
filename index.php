9cc734de5174a09077939a84fd764e10e52dd21b
MrMesquita <126338859+MrMesquita@users.noreply.github.com>
2023-03-24 09:52:54 -0300

    Agencia Mesquita
    Projeto básico cirando um site usando bootstrap

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Projeto Dev Web</title>
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+  <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
+</svg>
+</head>
+<body>
+ 
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" style="font-size: 24px;"href="#">Logo marca</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="">Home</a></li>
+          <li><a href="sobre">Sobre</a></li>
+          <li><a href="contato">Contato</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
+  <div class="box">
+
+    <section class="banner">
+      <div class="overlay"></div>
+      <div class="container chamada-banner">
+        <div class="row">
+            <div class="col-md-12 text-center">
+               <h2>Agência Mesquita</h2>
+               <p>Empresa voltada para desenvolvimento web marketing digital.</p>
+               <a href="">Saiba mais!</a>
+            </div><!--col-md-6--->
+        </div><!---row--->
+      </div>
+    </section><!---banner--->
+
+    <section class="cadastro-lead">
+        <div class="container ">
+            <div class="row text-center">
+                <div class="col-md-6">
+                    <h2><span class="glyphicon glyphicon-star"></span> Entre na nossa lista!</h2>
+                </div><!----col-md-6------>
+                <div class="col-md-6">
+                    <form method="post">
+                        <input type="text" placeholder="Seu melhor email" name="lista" /><input type="submit" />
+                    </form>
+                </div><!--col-md-6--->
+            </div><!---row---->
+        </div><!---container--->
+    </section><!----cadastro-lead------>
+
+    <section class="depoimento">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+              <h2>Depoimento</h2>
+              <blockquote>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo."</p>
+              </blockquote>
+          </div><!----col-md-12---->
+        </div><!-------row------->
+      </div><!-----container----->
+    </section><!-----depoimento---->
+
+    <section class="diferenciais text-center">
+        <h2>Conheça nossa empresa</h2>
+      <div class="container diferenciais-container">
+        <div class="row">
+          <div class="col-md-4">
+            <span class="glyphicon glyphicon-glass"></span>
+            <h3>Diferencial #1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+          </div><!---col-md-4---->
+
+          <div class="col-md-4">
+            <span class="glyphicon glyphicon-star"></span>
+            <h3>Diferencial #1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+          </div><!---col-md-4---->
+
+          <div class="col-md-4">
+            <span class="glyphicon glyphicon-heart"></span>
+            <h3>Diferencial #1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lorem varius, pellentesque ipsum convallis, suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+          </div><!---col-md-4---->          
+        </div><!-----row------->
+      </div><!----container----->
+    </section><!-----diferenciais----->
+
+    <section class="equipe">
+      <h2>Equipe</h2>
+        <div class="container equipe-container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <div class="user-picture">
+                                    <div class="user-picture-child"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                              <h3>Marcelo</h3>
+                              <p>suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+                            </div>
+                        </div>
+                    </div><!---equipe-single--->
+                </div><!---col-md-6--->
+
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <div class="user-picture">
+                                    <div class="user-picture-child"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                              <h3>Marcelo</h3>
+                              <p>suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+                            </div>
+                        </div>
+                    </div><!---equipe-single--->
+                </div><!---col-md-6--->
+
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <div class="user-picture">
+                                    <div class="user-picture-child"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                              <h3>Marcelo</h3>
+                              <p>suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+                            </div>
+                        </div>
+                    </div><!---equipe-single--->
+                </div><!---col-md-6--->
+
+                <div class="col-md-6">
+                    <div class="equipe-single">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <div class="user-picture">
+                                    <div class="user-picture-child"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                              <h3>Marcelo</h3>
+                              <p>suscipit neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis orci quam. Phasellus dictum erat at nibh bibendum, eget porta urna pretium. Maecenas vel augue massa. Nulla facilisi. Nulla a suscipit quam, eu pharetra justo.</p>
+                            </div>
+                        </div>
+                    </div><!---equipe-single--->
+                </div><!---col-md-6--->
+            </div><!-----row----->
+        </div><!----container---->
+    </section><!-----equipe------>
+
+    <section class="final-site">
+      <div class="container">
+        <div class="row">
+
+            <div class="col-md-6">
+              <h2>Fale conosco</h2>
+              <form>
+                  <div class="form-group">
+                    <label for="exampleFormControlInput1">Nome:</label>
+                    <input type="name" class="form-control" id="exampleFormControlInput1">
+                  </div><!---form-group---->
+                  <div class="form-group">
+                    <label for="exampleFormControlInput2">Email:</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput2">
+                  </div><!---form-group---->
+                  <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Mensagem:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div><!---form-group---->
+              </form>
+            </div><!---col-md-6--->
+
+            <div class="col-md-6">
+              <h2>Nossos Planos</h2>
+                <table class="table">
+                    <thead>
+                      <tr>                        
+                        <th scope="col">Plano Semanal</th>
+                        <th scope="col">Plano Diário</th>
+                        <th scope="col">Plano Anual</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>R$ 199,00</td>
+                        <td>R$ 299,00</td>
+                        <td>R$ 399,00</td>
+                      </tr>
+                      <tr>
+                        <td>R$ 199,00</td>
+                        <td>R$ 299,00</td>
+                        <td>R$ 399,00</td>
+                      </tr>
+                      <tr>
+                        <td>R$ 199,00</td>
+                        <td>R$ 299,00</td>
+                        <td>R$ 399,00</td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div><!---col-md-6--->
+        </div><!----row----->
+      </div><!-----container----->
+    </section><!---final-site---->
+
+    <footer>
+        <p class="text-center">Todos os Direito reservados!</p>
+    </footer>
+
+  </div><!------box------->
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
